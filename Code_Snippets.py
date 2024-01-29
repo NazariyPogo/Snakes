45ca3d17130c5d9d4d1d2b43836d70dcda57537b
NazariyPogo <nazariypogo@gmail.com>
2024-01-28 19:58:56 -0500

    ^

--- Code_Snippets.py
+++ Code_Snippets.py
@@ -1,21 +1,21 @@
 #This file contains random snippets of code ready for copy/pasting to avoid rewriting the same useful code everytime
 
 #Code used to take repeatedly as a user for input and make sure it is an integer
-def get_user_input():
+def get_user_input(request_text):
     while True:
-        decision = input("Enter a number: ")
+        decision = input(request_text)
         if decision.isdigit():
             decision = int(decision)
             return decision
         else:
             print("That is an invalid input, please enter a whole number!")
 
-def get_user_int_in_range(bot, top):
+def get_user_int_in_range(bot, top, request_text):
     while True:
-        decision = input("Enter a number: ")
+        decision = input(request_text)
         if decision.isdigit():
             decision = int(decision)
             if decision >= 1000 and decision <= 9999:
                 return decision
         else:
-            print("That is an invalid input, please enter a whole number!")
+            print(f"That is an invalid input, please enter a whole number between {bot} and {top}!")
